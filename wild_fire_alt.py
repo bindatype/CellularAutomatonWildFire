e0e5832157a78912994c055302e6283003300c95
Glen MacLachlan <bindatype@gmail.com>
2018-04-26 01:40:34 -0400

    Changed ignition algorithm. Instead of focusing on unburnt cells this verion focuses on burning cells and looks for unburnt cells around burning cells. An unburnt cell faces jeopardy N times for N burning neighbors, unlike the original model where N burning neighbors is only as likely to ignite a cell a 1 burning neighbor.

---
+++ wild_fire_alt.py
@@ -0,0 +1,97 @@
+from collections import namedtuple
+import random
+import sys
+import math
+import time
+import copy
+
+def printf(format, *args):
+    sys.stdout.write(format % args)
+
+def showforest(nx,ny,t):
+    for i in range(ny):
+        for j in range(nx):
+            if tnew[i][j].STATE == 'F':
+                printf('\033[41m'"%c "'\033[0m',tnew[i][j].STATE)
+            elif tnew[i][j].STATE == '^':
+                printf('\033[42m'"%c "'\033[0m',tnew[i][j].STATE)
+            elif tnew[i][j].STATE == '.':
+                printf('\033[100m'"%c "'\033[0m',tnew[i][j].STATE)
+            else:
+                printf('\033[0m'"%c "'\033[0m',tnew[i][j].STATE)
+        printf("\n")
+    printf('\x1b[2J\x1b[H')
+    time.sleep(1)
+    
+def showforest_persist(nx,ny,t):
+    for i in range(ny):
+        for j in range(nx):
+            if tnew[i][j].STATE == 'F':
+                printf('\033[41m'"%c "'\033[0m',tnew[i][j].STATE)
+            elif tnew[i][j].STATE == '^':
+                printf('\033[42m'"%c "'\033[0m',tnew[i][j].STATE)
+            elif tnew[i][j].STATE == '.':
+                printf('\033[100m'"%c "'\033[0m',tnew[i][j].STATE)
+            else:
+                printf('\033[0m'"%c "'\033[0m',tnew[i][j].STATE)
+        printf("\n")
+
+NX = 80
+NY = 80
+generations = 50
+Forest = namedtuple('Forest', 'STATE B I D')
+tnew = []
+
+for i in range(NY):
+    new = []
+    for j in range(NX):
+        #Node = Forest('E',random.random(),random.random(),random.random())
+        Node = Forest(' ',0.5,0.5,random.random())
+        new.append(Node)
+    tnew.append(new)
+
+
+# Fill forest with trees
+Dinit = 0.85
+for i in range(1,NY-1):
+    for j in range(1,NX-1):
+        if Dinit - tnew[i][j].D > 0:
+            tnew[i][j] = tnew[i][j]._replace(STATE='^')
+
+
+#Start a fire in the middle of the grid */
+# Constant source fire */ //	tnew[nx/2][ny/2].B = 1;
+tnew[NY//2][NX//2] = tnew[NY//2][NX//2]._replace(STATE = 'F');
+
+
+"""
+generation = 0
+ims=[]
+
+"""
+for i in range(generations):
+    t = copy.deepcopy(tnew)
+    print(id(t),id(tnew),id(t[0]),id(tnew[0]),id(new),id(t[0][0]))
+    for i in range(1,NY-1):
+        for j in range(1,NX-1):
+            if t[i][j].STATE == 'F':
+                # probe local neighborhood of burning cells for unburnt cells to ignite
+                for li in range(-1,2):
+                    for lj in range(-1,2):
+                        if not (li == 0 and lj == 0):
+                            if t[i+li][j+lj].STATE == '^':
+                                if abs(li+lj) == 1:    
+                                    if t[i+li][j+lj].I > random.random():
+                                        tnew[i+li][j+lj] = tnew[i+li][j+lj]._replace(STATE='F')
+                                else:
+                                    if t[i+li][j+lj].I/math.sqrt(2.) > random.random():
+                                        tnew[i+li][j+lj] = tnew[i+li][j+lj]._replace(STATE='F')
+                # if cell is burning see if it goes out. 
+                if t[i][j].B < random.random():
+                    tnew[i][j] = tnew[i][j]._replace(STATE='.')
+    showforest(NX,NY,tnew)
+
+                    
+showforest_persist(NX,NY,t)
+
+
